b352a5e9bc9a45e6ffe9ea592c41b58306301c06
kilavila <christer.kilavik@protonmail.com>
2024-03-08 20:35:59 +0100

    reg debug

--- lua/nvim-yoink.lua
+++ lua/nvim-yoink.lua
@@ -97,7 +97,7 @@ end
 local function select()
   local current_line = api.nvim_get_current_line()
   vim.fn.setreg('*', current_line)
-  vim.fn.setreg('"', current_line)
+  vim.fn.setreg('0', current_line)
   close_window()
 end
 
